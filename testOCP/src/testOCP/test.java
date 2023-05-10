e0f97c412b69ff33aaa1338fbce3e4e58bdb2882
= <jkarkarit@gmail.com>
2023-05-10 15:43:18 +0100

    "#106..#109 completed Pattern Matching - Subtype - Flow Matching"

--- testOCP/src/testOCP/test.java
+++ testOCP/src/testOCP/test.java
@@ -1,9 +1,54 @@
 package testOCP;
 
+/**
+ * @author Mars
+ *
+ */
+/**
+ * @author Mars
+ *
+ */
 public class test {
 
+	static void printIntegersOrNumbesGreaterThan5(Number number) {
+		// @formatter:off
+		if (number instanceof Integer data)
+			System.out.println(data);
+		
+		if (number instanceof Integer data && data.compareTo(5)>0)
+			System.out.println(data);
+		
+		
+		// does not compile: compiler cannot guarantee that data is
+		// an instance of Integer
+		/*
+		if (number instanceof Integer data || data.compareTo(5)>0)
+			System.out.println(data);
+		**/
+		
+		
+		
+		// @formatter:on
+
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello from testOCP and egit");
+
+		// @formatter:off
+		Integer valueInteger	= 1;
+		Number	valueNumber 	= 2;
+		
+		if (valueInteger instanceof Integer){System.out.println(1);}
+		if (valueNumber instanceof Integer data) {System.out.println(2);}
+
+		
+		valueInteger	= 123;
+		valueNumber 	= 456;
+		test.printIntegersOrNumbesGreaterThan5(valueInteger);
+		test.printIntegersOrNumbesGreaterThan5(valueNumber);
+		
+		// @formatter:on
+		
 	}
 
 }
